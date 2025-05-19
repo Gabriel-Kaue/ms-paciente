e0e14ca3ead9d0591f84072764351ece41db7433
Gabriel Alves <gabrielkauecaitan099@gmail.com>
2025-05-19 17:07:30 -0300

    Criação do projjeto

---
+++ ms-usuario/src/main/kotlin/br/com/msusuario/MsUsuarioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.msusuario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsUsuarioApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsUsuarioApplication>(*args)
+}
