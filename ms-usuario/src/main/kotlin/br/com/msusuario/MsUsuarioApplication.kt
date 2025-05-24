82f9e5c50489a6e805f4a78ce0486a4cac66bb9e
Gabriel Alves <gabrielkauecaitan099@gmail.com>
2025-05-24 17:07:28 -0300

    feature:tá conectando com o bd, e o localhost tá ok

--- ms-usuario/src/main/kotlin/br/com/msusuario/MsUsuarioApplication.kt
+++ ms-usuario/src/main/kotlin/br/com/msusuario/MsUsuarioApplication.kt
@@ -2,9 +2,15 @@ package br.com.msusuario
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class MsUsuarioApplication
+class MsUsuarioApplication{
+    @GetMapping
+    fun hello() = "Hello World"
+}
 
 fun main(args: Array<String>) {
     runApplication<MsUsuarioApplication>(*args)
---
+++ ms-usuario/src/main/resources/sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Usuario(
+    cpf varchar(11),
+
+);
